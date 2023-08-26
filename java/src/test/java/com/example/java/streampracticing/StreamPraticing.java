16f7b3bca98d9036990f71d7601d5e8ecce6e710
junho <wnsgh1357@gmail.com>
2023-08-27 03:03:34 +0900

    테스트 문법

--- java/src/test/java/com/example/java/streampracticing/StreamPraticing.java
+++ java/src/test/java/com/example/java/streampracticing/StreamPraticing.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,6 +17,7 @@ import static org.hamcrest.Matchers.is;
 
 public class StreamPraticing {
     private List<StreamDto> streamDtoList;
+    private Map<Long, String> streamDtoMap;
     @BeforeEach
     void init(){
         StreamDto streamDto1 = new StreamDto(1L, "철수");
@@ -24,15 +28,58 @@ public class StreamPraticing {
         streamDtoList.add(streamDto2);
         streamDtoList.add(streamDto3);
 
+        streamDtoMap = new HashMap<>();
+        streamDtoMap.put(1L, "철수");
+        streamDtoMap.put(2L, "짱구");
+        streamDtoMap.put(3L, "철수");
+
     }
-    //filter
+
+    //List Stream filter
     @Test
     void test(){
-        Assertions.assertThat(streamDtoList.stream()
-                .filter(a -> a.getName().equals("철수")).count()).isEqualTo(2) ;
-        assertThat(streamDtoList.stream()
-                .filter(a -> a.getName().equals("철수")).findFirst().get().getId(), is(1L));
+        long 철수몇명 = streamDtoList.stream()
+                .filter(a -> a.getName().equals("철수"))
+                .count();
+        Assertions.assertThat(철수몇명).isEqualTo(2) ;
+
+        Long 첫번째철수아이디 = streamDtoList.stream()
+                .filter(a -> a.getName().equals("철수"))
+                .findFirst().get()
+                .getId();
+        Assertions.assertThat(첫번째철수아이디).isEqualTo(1L);
     }
 
+    //Map Stream filter
+    @Test
+    void test2(){
+        long 철수몇명 = streamDtoMap.entrySet().stream()
+                .filter(streamDto -> streamDto.getValue().equals("철수"))
+                .count();
+        Assertions.assertThat(철수몇명).isEqualTo(2) ;
 
+        Long 첫번째철수아이디 = streamDtoMap.entrySet().stream()
+                .filter(streamDto -> streamDto.getValue().equals("철수"))
+                .findFirst().get()
+                .getKey();
+        Assertions.assertThat(첫번째철수아이디).isEqualTo(1L);
+    }
+
+    @Test
+    void test3(){
+        Map<String, Integer> names = new HashMap<>();
+        names.put("stir", 1);
+        names.entrySet().stream()
+                .map(s -> names.put("stir", 2));
+
+        //stir에 2 삽입을 했지만 최종 연산을 넣어주지 않아 실행하지 않음.
+        System.out.println();
+    }
+
+    /*
+     return segmentResponse.getResults()
+                .stream()
+                .map(SegmentDto::setSegmentDto)
+                .toList();
+     */
 }
